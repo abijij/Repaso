afafbca3bded4366618b1bedb5fad6f10f245bf3
abijij <105397465+abijij@users.noreply.github.com>
2024-01-04 17:59:36 -0600

    Repaso 04/01/2024

--- src/main/java/pt_1/Automovil.java
+++ src/main/java/pt_1/Automovil.java
@@ -1,2 +1,91 @@
-package pt_1;public class Automovil {
+package pt_1;
+
+public class Automovil {
+
+    private String fabricante;
+    private String modelo;
+    private String color = "Rojo";
+    private double cilindrada;
+    private int capacidadTanque = 40 ;
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getCilindrada() {
+        return cilindrada;
+    }
+
+    public int getCapacidadTanque() {
+        return capacidadTanque;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setCilindrada(double cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    public void setCapacidadTanque(int capacidadTanque) {
+        this.capacidadTanque = capacidadTanque;
+    }
+
+    public  String  detalle(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("auto.fabricante = " + this.fabricante);
+        sb.append("\nauto.modelo = " + this.modelo);
+        sb.append("\nauto.color = " + this.color);
+        sb.append("\nauto.cilindrada = " + this.cilindrada);
+
+        return sb.toString();
+    }
+
+    public String acelerar (int rpm){
+        return "el auto " + this.fabricante + " acelerando a " + rpm + " rpm";
+    }
+
+    public  String frenar(){
+        return this.fabricante + " " + this.modelo + " Freanado!";
+    }
+
+    public String acelerarFrenar (int rpm){
+        String acelerar = this.acelerar(rpm);
+
+        String frenar = this.frenar();
+
+        return acelerar + "\n" + frenar;
+    }
+
+    public float calcularCpnsumo ( int km, float porcentajeGasolina){
+
+        return km/(capacidadTanque* porcentajeGasolina);
+
+    }
+
+    public float calcularCpnsumo ( int km, int porcentajeGasolina){
+
+        return km/(capacidadTanque* (porcentajeGasolina/100f));
+
+    }
+
+
 }
--- src/main/java/pt_1/Automovil_Ejemplo.java
+++ src/main/java/pt_1/Automovil_Ejemplo.java
@@ -1,7 +1,35 @@
 package pt_1;
 
-public class Automovil {
+public class Automovil_Ejemplo {
     public static void main(String[] args) {
 
+
+        Automovil subaru = new Automovil();
+        subaru.setFabricante("Subaru");
+        subaru.setModelo("Impreza");
+        subaru.setCilindrada(2.0);
+        subaru.setColor("Blanco");
+
+
+        Automovil mazda = new Automovil();
+        mazda.setFabricante("Mazda");
+        mazda.setModelo("BT-50");
+        mazda.setCilindrada(3.0);
+        mazda.setColor("Rojo");
+
+        System.out.println(subaru.detalle());
+        System.out.println(mazda.detalle());
+
+        System.out.println(subaru.acelerar(200));
+        System.out.println(mazda.acelerar(300));
+
+        System.out.println( subaru.frenar());
+        System.out.println( mazda.frenar());
+
+        System.out.println(subaru.acelerarFrenar(200));
+        System.out.println(mazda.acelerarFrenar(300));
+
+        System.out.println("Kilometros por litro " + subaru.calcularCpnsumo(300, 0.75f));
+        System.out.println("Kilometros por litro " + subaru.calcularCpnsumo(300, 75));
     }
 }
